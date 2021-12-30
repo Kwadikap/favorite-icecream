8088d244aeab2e043eeb5b5ea4f3eb79a9b6144d
Kwadwo <kwadikap@gmail.com>
2021-12-30 15:51:32 -0500

    Create server

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require('express');
 const server = express();
 
 const PORT = process.env.PORT || 3000;
+
+
+
+server.listen(PORT, () => console.log( `Server listening on port: ${PORT}`));
